8e487fed5dbe07387403fc9327fc4da53a7713ec
Andrei Klimovich <andrey.klimovich98@gmail.com>
2022-05-09 14:44:01 +0300

    feature/WA-22 Added connection with user service

--- src/main/java/com/weather_alligator/forecast_service/config/ForecastServiceConfig.java
+++ src/main/java/com/weather_alligator/forecast_service/config/ForecastServiceConfig.java
@@ -1,6 +1,8 @@
 package com.weather_alligator.forecast_service.config;
 
 import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,5 +18,12 @@ public class ForecastServiceConfig {
     @Bean("jsonConverter")
     public Jackson2JsonMessageConverter jsonConverter() {
         return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        var rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(jsonConverter());
+        return rabbitTemplate;
     }
 }
--- src/main/java/com/weather_alligator/forecast_service/transport/listener/ForecastListener.java
+++ src/main/java/com/weather_alligator/forecast_service/transport/listener/ForecastListener.java
@@ -1,14 +1,34 @@
 package com.weather_alligator.forecast_service.transport.listener;
 
 import com.weather_alligator.forecast_service.transport.model.ForecastRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
+@RequiredArgsConstructor
 public class ForecastListener {
-    
+
+    public static final String USER_SERVICE_LOCATION_RK = "user_service.location";
+    public static final String USER_SERVICE_EXCHANGE = "userServiceExchange";
+
+    private final RabbitTemplate rabbitTemplate;
+
     @RabbitListener(queues = "forecastQueue", messageConverter = "jsonConverter")
     public void getForecast(ForecastRequest request) {
-        // TODO implement service call and delete this comment
+        log.info("Received forecast request: {}", request);
+
+        var locationRequest = new LocationRequest(request.getUserId());
+        log.info("Sending location request: {}", locationRequest);
+        var locationResponse = (LocationResponse) rabbitTemplate.convertSendAndReceiveAsType(USER_SERVICE_EXCHANGE, USER_SERVICE_LOCATION_RK, locationRequest, ParameterizedTypeReference.forType(LocationResponse.class));
+        log.info("Received location response: {}", locationResponse);
+        // TODO add handling of request and send response
     }
+
+    record LocationRequest(Long userId) {}
+    record LocationResponse(Float lat, Float lon) {}
 }
